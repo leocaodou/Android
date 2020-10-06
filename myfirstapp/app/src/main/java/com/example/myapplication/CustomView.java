c148daf4aeb65c57a3d6c5fed0f377bb1e764eac
leocaodou <1210579369@qq.com>
2020-10-06 23:50:01 +0800

    作业

--- myfirstapp/app/src/main/java/com/example/myapplication/CustomView.java
+++ myfirstapp/app/src/main/java/com/example/myapplication/CustomView.java
@@ -1,15 +1,20 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class CustomView extends View {
-
+    private float startX = -1;
+    private float startY = -1;
+    private float endX = -1;
+    private float endY = -1;
     public CustomView(Context context, AttributeSet attrs) {
         super(context,attrs);
     }
@@ -17,25 +22,20 @@ public class CustomView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
-        Paint paint=new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(5);
-        canvas.drawRect(10, 10, 100, 100, paint);
-        Path path=new Path();
-        path.moveTo(10, 120);
-        path.lineTo(200, 120);
-        path.lineTo(100, 200);
-        path.close();
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawPath(path, paint);
-        paint.setTextSize(12);
-        paint.setColor(Color.BLUE);
-        paint.setStrikeThruText(true);
-        canvas.drawText("hello world", 10, 200, paint);
-        Path pathText=new Path();
-        pathText.addCircle(200,300,80, Path.Direction.CCW);
-        canvas.drawTextOnPath("Draw the text, with origin at (x,y), using the specified paint, along the specified path.",pathText,0,10,paint);
+        
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            if(startX == -1 && startY == -1){
+                startX = event.getX();
+                startY = event.getY();
+            }
+        }
+        if(event.getAction() == MotionEvent.ACTION_UP){
+            endX = event.getX();
+            endY = event.getY();
+
+        }
     }
 }
