5f74ab7edd95ebd16ff9edbea7aeef0050387813
leocaodou <1210579369@qq.com>
2020-10-07 23:52:16 +0800

    homework

--- myfirstapp/app/src/main/java/com/example/myapplication/CustomView.java
+++ myfirstapp/app/src/main/java/com/example/myapplication/CustomView.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -15,27 +17,42 @@ public class CustomView extends View {
     private float startY = -1;
     private float endX = -1;
     private float endY = -1;
+    private Bitmap bitmap = null;
     public CustomView(Context context, AttributeSet attrs) {
         super(context,attrs);
+        bitmap = Bitmap.createBitmap(getWidth(),getHeight(),Bitmap.Config.ARGB_8888);
     }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
-        
+        canvas.drawBitmap(HandWriting(bitmap), 0, 0,null);
     }
-
+    public Bitmap HandWriting(Bitmap bm){
+        Canvas canvas =new Canvas(bm);
+        Paint paint = new Paint();
+        if(startX != -1 && startY != -1) {
+            canvas.drawLine(startX, startY, endX, endY, paint);
+            startX = -1;
+            startY = -1;
+        }
+        return bm;
+    }
     public boolean onTouchEvent(MotionEvent event){
         if(event.getAction() == MotionEvent.ACTION_DOWN){
             if(startX == -1 && startY == -1){
                 startX = event.getX();
                 startY = event.getY();
+                invalidate();
+                return true;
             }
         }
-        if(event.getAction() == MotionEvent.ACTION_UP){
+        else if(event.getAction() == MotionEvent.ACTION_UP){
             endX = event.getX();
             endY = event.getY();
-
+            invalidate();
+            return true;
         }
+        return super.onTouchEvent(event);
     }
 }
